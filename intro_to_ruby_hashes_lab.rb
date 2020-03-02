22f6d27fbc251a3eabe82ffc652ba1f8e58c22d5
Taylor Gonzalez <taylorgon@gmail.com>
2020-03-02 03:09:48 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,8 +32,7 @@ def monopoly_with_third_tier
       }, 
       :names => 
       {
-        :reading_railroad => 
-        {
+        :reading_railroad => {
         },
         :pennsylvania_railroad => {
         },
