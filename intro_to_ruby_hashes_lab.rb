d731e0a67f601b53820686f967ec88fe92cb91c6
Taylor Gonzalez <taylorgon@gmail.com>
2020-03-02 02:00:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,7 +3,7 @@ def base_hash
     :railroads => {}
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.first_challenge
-  monopoly{}
+   monopoly{}
 end
 
 def monopoly_with_second_tier
