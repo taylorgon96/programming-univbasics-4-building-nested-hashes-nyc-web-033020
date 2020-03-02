8c53ffb550e09db33790461daff8393355906fee
Taylor Gonzalez <taylorgon@gmail.com>
2020-03-02 03:12:16 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -77,9 +77,10 @@ def monopoly_with_fourth_tier
         {
           "mortgage_value"=>"$400"
         },
-        :shortline_railroad => {
-        }
-      
+        :shortline_railroad => 
+        {
+          "mortgage_value"=>"$800"
+          }
         }
       }
     }
