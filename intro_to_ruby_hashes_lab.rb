dab13a530fa2f10d90d6aa810bea0d15a740025a
Taylor Gonzalez <taylorgon@gmail.com>
2020-03-02 02:32:43 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,18 +23,7 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-  onopoly = {
-    :railroads =>  
-    {
-      :pieces => 4,
-      :rent_in_dollars => 
-      {
-          :rent_in_dollars =>
-      {
-        :one_piece_owned => 25,
-        :two_pieces_owned => 50,
-        :three_pieces_owned=>100,
-        :four_pieces_owned=>200
+ 
     
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
