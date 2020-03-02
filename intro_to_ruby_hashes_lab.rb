937c23325fce3c4c527929ed05bc0b516115324c
Taylor Gonzalez <taylorgon@gmail.com>
2020-03-02 02:05:43 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,8 +1,8 @@
 def base_hash 
   monopoly =  {
     :railroads => {}
-  # Remember implicit return! We're returning this new Hash _without_ the
-  # keyword "return." Nice and neat.first_challenge
+                    # Remember implicit return! We're returning this new Hash _without_ the
+                     # keyword "return." Nice and neat.first_challenge
   }
   monopoly
 end
