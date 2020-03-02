5d9acb2c9044cfd7f7348484e17f658290a5b56b
Taylor Gonzalez <taylorgon@gmail.com>
2020-03-02 02:51:48 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -36,7 +36,7 @@ def monopoly_with_third_tier
         :pennsylvania_railroad => {}
         :b_and_o_railroad => {}
         :shortline => {}
-      }
+      
     }
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
