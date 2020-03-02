3ee72463e52d3dfa683fb64fb185a09f0e7970aa
Taylor Gonzalez <taylorgon@gmail.com>
2020-03-02 02:29:45 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,6 +16,9 @@ def monopoly_with_second_tier
       {
         :one_piece_owned => 25,
         :two_pieces_owned => 50,
+          :three_pieces_owned=>100,
+      :four_pieces_owned=>200
+    },
       }
     }
   # When you start writing the implementation for this method, copy the Hash
